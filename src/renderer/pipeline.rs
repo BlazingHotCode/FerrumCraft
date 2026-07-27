b3f5525523aa4d920c4748887ed5e8bf8fa8f82c
BlazingHotCode <nadav.babad@gmail.com>
2026-07-27 12:56:09 +0300

    fix(renderer): expose camera uniform to fog shader

--- src/renderer/pipeline.rs
+++ src/renderer/pipeline.rs
@@ -63,7 +63,7 @@ impl RenderPipelines {
                 label: Some("Camera bind group layout"),
                 entries: &[wgpu::BindGroupLayoutEntry {
                     binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX,
+                    visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
                     ty: wgpu::BindingType::Buffer {
                         ty: wgpu::BufferBindingType::Uniform,
                         has_dynamic_offset: false,
